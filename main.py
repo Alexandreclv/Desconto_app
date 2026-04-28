6308a8eed6b2184f8fe397b984176661e26d0efc
AlexandreCardim <alexandre.vasconcellos@aln.senaicimatec.edu.br>
2026-04-28 14:28:38 -0300

    Arquitetura limpa: estruturando as camadas.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from src.services.pedido_service import PedidoService
-from src.models.desconto import DescontoNormal, DescontoVIP, DescontoPremium
-from src.models.pedido import Pedido
+from src_antigo.services.pedido_service import PedidoService
+from src_antigo.models.desconto import DescontoNormal, DescontoVIP, DescontoPremium
+from src_antigo.models.pedido import Pedido
 
 if __name__ == "__main__":
     service = PedidoService()
