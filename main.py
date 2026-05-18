0088f3218ab07043a38f528b4942c1510d77c8e2
Alexandre Vasconcellos <xande.vasconcellos2@gmail.com>
2026-05-18 12:28:16 -0300

    Arquitetura limpa: Implementação de todo fluxo e chamada na main.

--- main.py
+++ main.py
@@ -2,28 +2,38 @@ from src.app.adapters.repositories.memory_pedido_repository import MemoryPedidoR
 from src.app.frameworks.database.memory_database import MemoryDatabase
 from src.app.use_cases.criar_pedido import CriarPedido
 from src.app.adapters.controllers.pedido_controller import PedidoController
+from src.app.presenters.pedido_presenter import PedidoPresenter
 
 
-def build_controller() -> PedidoController:
-    database = MemoryDatabase()
-    repository = MemoryPedidoRepository(database)
-    use_case = CriarPedido(repository)
-    return PedidoController(use_case)
+
 
 def main() -> None:
-    controller = build_controller()
+    database = MemoryDatabase()
 
-    controller.criar_pedido("Cliente A", 100.0, "normal")
-    controller.criar_pedido("Cliente B", 200.0, "vip")
-    controller.criar_pedido("Cliente C", 300.0, "premium")
+    pedido_gateway = MemoryPedidoRepository(database)
 
+    criar_pedido_use_case = CriarPedido(pedido_gateway)
+
+    presenter = PedidoPresenter()
+
+    controller = PedidoController(
+        criar_pedido_use_case = criar_pedido_use_case, 
+        presenter = presenter
+    )
+
+    pedido1 = controller.criar_pedido("Cliente A", 100.0, "normal")
+    pedido2 = controller.criar_pedido("Cliente B", 200.0, "premium")
+    pedido3 = controller.criar_pedido("Cliente C", 300.0, "vip")
+
+    print("Pedidos Criados:")
+    print(pedido1)
+    print(pedido2)
+    print(pedido3)
+
+    print("\nPedidos Salvos:")
     for pedido in controller.listar_pedidos():
-        print(
-            f"Cliente: {pedido.cliente} | "
-            f"Valor original: R$ {pedido.valor_original:.2f} | "
-            f"Desconto: R$ {pedido.valor_desconto:.2f} | "
-            f"Valor final: R$ {pedido.valor_final:.2f}"
-        )
+        print(pedido)
 
+        
 if __name__ == "__main__":
     main()
